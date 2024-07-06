1804d860da437dd8332abd6fe4e1be8f386bb618
seung365 <bdh3659@naver.com>
2024-07-07 02:53:58 +0900

    design: 미디어쿼리 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import "@/styles"
 import ReactDOM from "react-dom/client"
 import App from "@/App"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+import "@/install"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(<App />)
